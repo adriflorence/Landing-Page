c256c490537707c9f46bc33cc631fc7b0bfc34d0
Adri Forczek <forczekadri@gmail.com>
2020-05-10 23:18:41 +0100

    chore: adds button to go to top once halfway through the screen

--- js/app.js
+++ js/app.js
@@ -11,6 +11,8 @@
  * 
  * JS Standard: ESlint
  * 
+ * All content sourced from: https://stackify.com/solid-design-principles/
+ * 
 */
 
 /**
@@ -42,6 +44,26 @@ function isElementInViewport (el) {
     );
 }
 
+// scroll to top button on the page once user is below the fold
+function isPassedFold(){
+    document.addEventListener('scroll', function() {
+        let fold = document.body.clientHeight / 2;
+        let user_location = window.innerHeight + window.scrollY;
+        let toTopButton = document.getElementById('to_top');
+        if (user_location > fold) {
+            toTopButton.style.display = "block";
+        } else {
+            toTopButton.style.display = "none";
+        }
+    });
+}
+
+// Scroll to top of the page
+// Source: https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+function backToTop(){
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 
 // Scroll to section on link click using scrollTO event
 function scrollToElement() {
@@ -51,6 +73,7 @@ function scrollToElement() {
         clicked.scrollIntoView();
     });
 };
+
 
 /**
  * End Helper Functions
@@ -80,18 +103,26 @@ function buildMenu(){
     navbar.appendChild(fragment);
 }
 
+// Add a scroll to top button on the page
+// that’s only visible when the user scrolls below the fold
+const toTopButton = document.getElementById('to_top');
+toTopButton.onclick = function() { backToTop() };
+
+
 
 // Add functionality to distinguish the section in view
 // Add class 'active' to section when near top of viewport
-document.addEventListener('scroll', function() {
-    for(section of sections){
-        if(isElementInViewport(section)){
-            section.classList = "your-active-class";
-        } else {
-            section.classList = "";
+function setActive() {
+    document.addEventListener('scroll', function() {
+        for(section of sections){
+            if(isElementInViewport(section)){
+                section.classList = "your-active-class";
+            } else {
+                section.classList = "";
+            }
         }
-    }
-});
+    });
+}
 
 
 /**
@@ -105,3 +136,9 @@ buildMenu();
 
 // Scroll to section on link click
 scrollToElement();
+
+// Set sections as active
+setActive();
+
+// Scroll to top button
+isPassedFold();
