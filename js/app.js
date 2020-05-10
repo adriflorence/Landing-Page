586947f9aaebc13a10259e58833ae11580ec86f9
Adri Forczek <forczekadri@gmail.com>
2020-05-10 14:19:56 +0100

    chore: adds functionality to scroll to sections

--- js/app.js
+++ js/app.js
@@ -18,6 +18,8 @@
  * 
 */
 
+const landing__containers = document.getElementsByClassName('landing__container');
+const navbar__list = document.getElementById('navbar__list');
 
 /**
  * End Global Variables
@@ -34,6 +36,28 @@
 */
 
 // build the nav
+const fragment = document.createDocumentFragment();
+
+// traditional for loop is used so the index can be added to the href attribute
+for(let i = 0; i < landing__containers.length; i++){
+    let section = landing__containers[i];
+    let index = i + 1;
+    let title = section.querySelector('h2').textContent;
+    let listElement = document.createElement('li');
+
+    // create anchor tag and with section title text
+    let anchor = document.createElement('a');
+    anchor.setAttribute('href', '#section' + index);
+    anchor.classList.add('menu__link');
+    anchor.textContent = title;
+
+    // append anchor to list element, and list element to document fragment
+    listElement.appendChild(anchor);
+    fragment.appendChild(listElement);
+}
+
+navbar__list.appendChild(fragment);
+
 
 
 // Add class 'active' to section when near top of viewport
